120fa8053df9d41503c27b34022508d34eb39598
Md. Sazzadul Islam <sazib66@gmail.com>
2017-03-29 00:17:51 +0600

    pushing

--- js/chart-data.js
+++ js/chart-data.js
@@ -143,16 +143,16 @@ window.onload = function(){
 	});
 	
 
-	var chart2 = document.getElementById("bar-chart").getContext("2d");
-	window.myBar = new Chart(chart2).Bar(barChartData, {
-		responsive : true
-	});
-	var chart3 = document.getElementById("doughnut-chart").getContext("2d");
-	window.myDoughnut = new Chart(chart3).Doughnut(doughnutData, {responsive : true
-	});
-	var chart4 = document.getElementById("pie-chart").getContext("2d");
-	window.myPie = new Chart(chart4).Pie(pieData, {responsive : true
-	});
+	// var chart2 = document.getElementById("bar-chart").getContext("2d");
+	// window.myBar = new Chart(chart2).Bar(barChartData, {
+	// 	responsive : true
+	// });
+	// var chart3 = document.getElementById("doughnut-chart").getContext("2d");
+	// window.myDoughnut = new Chart(chart3).Doughnut(doughnutData, {responsive : true
+	// });
+	// var chart4 = document.getElementById("pie-chart").getContext("2d");
+	// window.myPie = new Chart(chart4).Pie(pieData, {responsive : true
+	// });
 
 	var respondentByGenderVar = document.getElementById("piechartsMF").getContext("2d");
 	window.myPie = new Chart (respondentByGenderVar).Pie(pieData2, {responsive: true});

cc5857f937bcfdf5783aed4213df0812e6d48f93
Md. Sazzadul Islam <sazib66@gmail.com>
2017-03-28 22:13:11 +0600

    js issue

--- js/chart-data.js
+++ js/chart-data.js
@@ -1,60 +1,53 @@
 var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 	
 	var lineChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : ["Dhaka","Bogura","Shunamgonj"],
 			datasets : [
 				{
 					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				},
-				{
-					label: "My Second dataset",
 					fillColor : "rgba(48, 164, 255, 0.2)",
-					strokeColor : "rgba(48, 164, 255, 1)",
+					strokeColor :  "rgba(48, 164, 255, 1)",
 					pointColor : "rgba(48, 164, 255, 1)",
 					pointStrokeColor : "#fff",
 					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(48, 164, 255, 1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				}
-			]
-
-		}
-			var lineChartData2 = {
-			labels : ["January","February","Msdasarch","Asdsapril","sdMay","June","July"],
-			datasets : [
-				{
-					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
 					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+					data : [500,70,150]
 				},
-				{
-					label: "My Second dataset",
-					fillColor : "rgba(48, 164, 255, 0.2)",
-					strokeColor : "rgba(48, 164, 255, 1)",
-					pointColor : "rgba(48, 164, 255, 1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(48, 164, 255, 1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				}
+				
 			]
 
 		}
+			
+		// var lineChartDatatest = {
+		// 	labels : ["January","February","March","April","May","June","July"],
+		// 	datasets : [
+		// 		{
+		// 			label: "My First dataset",
+		// 			fillColor : "rgba(220,220,220,0.2)",
+		// 			strokeColor : "rgba(220,220,220,1)",
+		// 			pointColor : "rgba(220,220,220,1)",
+		// 			pointStrokeColor : "#fff",
+		// 			pointHighlightFill : "#fff",
+		// 			pointHighlightStroke : "rgba(220,220,220,1)",
+		// 			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+		// 		},
+		// 		{
+		// 			label: "My Second dataset",
+		// 			fillColor : "rgba(48, 164, 255, 0.2)",
+		// 			strokeColor : "rgba(48, 164, 255, 1)",
+		// 			pointColor : "rgba(48, 164, 255, 1)",
+		// 			pointStrokeColor : "#fff",
+		// 			pointHighlightFill : "#fff",
+		// 			pointHighlightStroke : "rgba(48, 164, 255, 1)",
+		// 			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+		// 		}
+		// 	]
+
+		// }
+			
 		
 	var barChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : ["Male","February","March","April","May","June","July"],
 			datasets : [
 				{
 					fillColor : "rgba(220,220,220,0.5)",
@@ -101,6 +94,19 @@ var randomScalingFactor = function(){ return Math.round(Math.random()*1000)};
 				}
 
 			];
+			var pieData2 = [
+				{
+					value: 300,
+					color:"#30a5ff",
+					highlight: "#62b9fb",
+					label: "Male"
+				},
+				{
+					value: 50,
+					color: "#ffb53e",
+					highlight: "#fac878",
+					label: "Female"
+				}];
 			
 	var doughnutData = [
 					{
@@ -135,10 +141,8 @@ window.onload = function(){
 	window.myLine = new Chart(chart1).Line(lineChartData, {
 		responsive: true
 	});
-	var chart2 = document.getElementById("line-chart2").getContext("2d");
-	window.myLine2 = new Chart(chart2).Line(lineChartData2, {
-		responsive: true
-	});
+	
+
 	var chart2 = document.getElementById("bar-chart").getContext("2d");
 	window.myBar = new Chart(chart2).Bar(barChartData, {
 		responsive : true
@@ -149,5 +153,8 @@ window.onload = function(){
 	var chart4 = document.getElementById("pie-chart").getContext("2d");
 	window.myPie = new Chart(chart4).Pie(pieData, {responsive : true
 	});
+
+	var respondentByGenderVar = document.getElementById("piechartsMF").getContext("2d");
+	window.myPie = new Chart (respondentByGenderVar).Pie(pieData2, {responsive: true});
 	
 };
